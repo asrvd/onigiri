744c316c40beb411dc7321ae76893801b954411f
ashish <ashirvadbhushan@gmail.com>
2022-05-11 08:25:09 +0530

    Fix Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute bottom-0 w-5/6 p-4 right-0 lg:p-8 md:p-8 md:right-auto lg:right-auto text-right text-zinc-300">
+    <div class="absolute bottom-0 w-5/6 p-4 right-0 lg:pr-8 lg:pb-0 md:p-4 md:pb-0 md:right-0 lg:right-auto text-right text-zinc-300">
         <h2>🍙 onigiri by <a href="https://github.com/asheeeshh" class="text-zinc-100 font-bolder">ashish.</a></h2>
     </div>
 </template>
