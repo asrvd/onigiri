7230171268ad0b4e1b4bd5d45257e8c6af46a7f1
ashish <68690233+asrvd@users.noreply.github.com>
2022-07-21 15:02:33 +0530

    update username

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,6 +11,6 @@ export default {
 
 <template>
     <div class="bottom-0 w-5/6 p-4 right-0 lg:pr-8 lg:pb-0 md:p-4 md:pb-0 md:right-0 lg:right-auto text-right" :class="config.showFooter ? 'absolute' : 'hidden'" :style="{color: config.footerTextColor}">
-        <h2>🍙 onigiri by <a href="https://github.com/asheeeshh" class="font-[500] underline decoration-dotted">ashish.</a></h2>
+        <h2>🍙 onigiri by <a href="https://github.com/asrvd" class="font-[500] underline decoration-dotted">ashish.</a></h2>
     </div>
 </template>
